e77d0c6c3c7e049e6a53ca651bf16720fa470773
Randi Guarcas <randi_guarcas18@hotmail.com>
2017-05-24 00:10:05 -0600

    big structure. first logic

---
+++ src/classes/First.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Randi Guarcas
+ */
+public class First {
+    public String key;
+    public ArrayList<String> value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public ArrayList<String> getValue() {
+        return value;
+    }
+
+    public void setValue(ArrayList<String> value) {
+        this.value = value;
+    }
+    
+   
+}
---
+++ src/classes/Struct.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Randi Guarcas
+ */
+public class Struct {
+    public String key;
+    public ArrayList< ArrayList<String> > value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public ArrayList<ArrayList<String>> getValue() {
+        return value;
+    }
+
+    public void setValue(ArrayList<ArrayList<String>> value) {
+        this.value = value;
+    }
+    
+    
+    
+    
+}
